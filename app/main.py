c237eca73ab49f2899b1083b78287ebc93611ced
EC2 Default User <ec2-user@ip-10-0-0-151.ap-south-2.compute.internal>
2026-04-16 13:01:13 +0000

    4.16 집 가기 전 푸시

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from routers.members import router
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(root_path="/management")
+# FastAPI가 자신이 /management 하위에서 동작한다는 걸 인식하는 용도 = root_path 설정
+app = FastAPI(root_path="/management") # root_path는 경로 변환에 관여하지 않고, nginx의 proxy_pass 끝 슬래시가 경로 변환을 담당
 app.include_router(router)
 
 @app.get("/health")
